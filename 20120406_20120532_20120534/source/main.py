bd73f94e5425a263c313bfeb59e4bf85505d0704
Pham Quoc Vuong <82097799+arren16@users.noreply.github.com>
2022-10-23 20:29:47 +0700

    Fix main

--- 20120406_20120532_20120534/source/main.py
+++ 20120406_20120532_20120534/source/main.py
@@ -1,3 +1,4 @@
+from imp import reload
 import mazeDrawing as mD
 import dfs
 import bfs
@@ -5,12 +6,41 @@ import ucs
 import greedy
 import A_star
 import matplotlib.pyplot as plt
+import sys
 
 def main():
-    for k in '12345':
+    mainFileNameLen = len("/source/main.py")
+    _SH_FILE_DIR_ = sys.argv[0] # .../source/main.py
+    
+    # folder contain "run.sh" (no '/' character at the end)
+    _SH_FILE_DIR_ = _SH_FILE_DIR_[:-mainFileNameLen]
+
+    with open(f'{_SH_FILE_DIR_}/source/dir.py', 'w') as file:
+        file.write(f'_SH_FILE_DIR_ = "{_SH_FILE_DIR_}"')
+
+    import dir
+    reload(dir)
+
+    outputDir = f'{dir._SH_FILE_DIR_}/output'
+
+    with open(f'{_SH_FILE_DIR_}/level1_InputFilenames.txt') as f:
+        level1_InputFilenames = f.readlines()
+
+    with open(f'{_SH_FILE_DIR_}/level2_InputFilenames.txt') as f:
+        level2_InputFilenames = f.readlines()
+    
+    with open(f'{_SH_FILE_DIR_}/advance_InputFilenames.txt') as f:
+        advance_InputFilenames = f.readlines()
+    
+    level1_InputFilenames = [filename[:-1] for filename in level1_InputFilenames]
+    level2_InputFilenames = [filename[:-1] for filename in level2_InputFilenames]
+    advance_InputFilenames = [filename[:-1] for filename in advance_InputFilenames]
+
+    for inputFileName in level1_InputFilenames:
+        minimalInputFileName = inputFileName.split('/')[-1].split('.txt')[0]
+
         #Get matrix
-        inputFile = '/level_1/input' + k
-        matrix = mD.read_file('input' + inputFile + '.txt')
+        matrix = mD.read_file(inputFileName)
 
         start = [0,0]
         end = [0,0]
@@ -28,39 +58,30 @@ def main():
                     pass
 
 
-        #DFS        
-        ax=plt.figure()
-        res = dfs.DFS(matrix, start, end)
-        mD.visualize_maze(ax, matrix,start,end, 'output' + inputFile + '/dfs',res)
+        # #DFS
+        
+        # ax=plt.figure()
+        # res = dfs.DFS(matrix, start, end)
+        # mD.visualize_maze(ax, matrix,start,end, outputDir + '/level_1/' + minimalInputFileName + '/dfs',res)
 
         # BFS
         ax=plt.figure()
         res = bfs.bfs(matrix, start, end)
-        mD.visualize_maze(ax, matrix,start,end, 'output' + inputFile + '/bfs',res)
+        mD.visualize_maze(ax, matrix,start,end, outputDir + '/level_1/' + minimalInputFileName + '/bfs',res)
 
-        # UCS
+        # # UCS
+        # ax=plt.figure()
+        # res = ucs.UCS(matrix, start, end)
+        # mD.visualize_maze(ax, matrix,start,end, outputDir + '/level_1/' + minimalInputFileName + '/ucs',res)
+
+        # Greedy
         ax=plt.figure()
-        res = ucs.UCS(matrix, start, end)
-        mD.visualize_maze(ax, matrix,start,end, 'output' + inputFile + '/ucs',res)
+        res = greedy.greedy(matrix, start, end)
+        mD.visualize_maze(ax, matrix,start,end, outputDir + '/level_1/' + minimalInputFileName + '/Greedy',res)
 
-        # Greedy 1
-        ax=plt.figure()
-        res = greedy.greedy(matrix, start, end, 1)
-        mD.visualize_maze(ax, matrix,start,end, 'output' + inputFile + '/gbfs1',res)
-
-        # Greedy 2
-        ax=plt.figure()
-        res = greedy.greedy(matrix, start, end, 1)
-        mD.visualize_maze(ax, matrix,start,end, 'output' + inputFile + '/gbfs2',res)
-
-        # A* with first heuristic
+        #A*
         ax=plt.figure()
         res = A_star.aStar(matrix, start, end)
-        mD.visualize_maze(ax, matrix,start,end, 'output' + inputFile + '/astar1',res)
-
-        # A* with second heuristic
-        ax=plt.figure()
-        res = A_star.aStar(matrix, start, end, 1)
-        mD.visualize_maze(ax, matrix,start,end, 'output' + inputFile + '/astar2',res)
+        mD.visualize_maze(ax, matrix,start,end, outputDir + '/level_1/' + minimalInputFileName + '/A_star',res)
 
 main()
